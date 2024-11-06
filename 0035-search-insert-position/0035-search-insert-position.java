214b0701faf6fddaa884f1b8a829b20502bb5748
Anmol Gupta <2k23csds2310397@gmail.com>
2024-11-06 09:22:31 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (6.72%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int mid=0,low,high;
+        low=0;
+        high=nums.length-1;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+                
+        }
+        return low;
+    }
+}
