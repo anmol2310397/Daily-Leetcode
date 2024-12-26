c60607ac9b81730ca2535e00c8ace8e5482cc05d
Anmol Gupta <2k23csds2310397@gmail.com>
2024-12-26 22:10:23 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (16.52%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,19 +1,8 @@
 class Solution {
-    public int add(int n){
-        int n1=n,sum=0;
-        while(n1>0){
-            sum+=n1%10;
-            n1/=10;
-        }
-        return sum;
-    }
     public int addDigits(int num) {
-        if(num<9)
+        if(num>=0 && num<=9)
             return num;
-        int ans=add(num);
-        while(ans>9){
-        ans=add(ans);
-        }
-        return ans;
+        else
+           return num%9 == 0 ? 9 : num%9 ;
     }
 }
