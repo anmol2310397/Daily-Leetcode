55a006f45ebe36080e1c604ff459b10566d6744d
Anmol Gupta <2k23csds2310397@gmail.com>
2025-05-29 22:51:58 +0530

    Time: 16 ms (80.94%), Space: 45.2 MB (50.28%) - LeetHub

---
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        int maxFreq = 0;
+        int result = -1;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int freq = entry.getValue();
+
+            if (freq > maxFreq || (freq == maxFreq && key < result)) {
+                maxFreq = freq;
+                result = key;
+            }
+        }
+
+        return result;
+    }
+}
