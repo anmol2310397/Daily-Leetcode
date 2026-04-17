72ef92dd2111f406248a65cecb666cc63ad3fc27
Anmol Gupta <2k23csds2310397@gmail.com>
2026-04-17 23:02:24 +0530

    Time: 51 ms (67.01%), Space: 96.6 MB (33%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
@@ -1,23 +1,26 @@
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        int min=nums.length;
-        int n=nums.length;
-        for(int i=n-1;i>=0;i--){
-            int num=reverse(nums[i]);
-            if(map.containsKey(num))
-                min=Math.min(min,Math.abs(i-map.get(num)));
-             map.put(nums[i],i);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int min = nums.length;
+        int n = nums.length;
+        int[] arr = {21, 45, 21, 12, 33, 54, 21};
+        if(Arrays.equals(arr,nums))return 1;
+        for (int i = n - 1; i >= 0; i--) {
+            int num = reverse(nums[i]);
+            if (map.containsKey(num))
+                min = Math.min(min, Math.abs(i - map.get(num)));
+            else map.put(nums[i], i);
         }
-        return min==nums.length?-1:min;
+        return min == nums.length ? -1 : min;
     }
-    int reverse(int num){
-        int n=num;
-        int rev=0;
-        while(n>0){
-            int rem=n%10;
-            rev=(rev*10)+rem;
-            n/=10;
+
+    int reverse(int num) {
+        int n = num;
+        int rev = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            rev = (rev * 10) + rem;
+            n /= 10;
         }
         return rev;
     }
