2838c312fa298e06dcfb7fedc2a29505298415b2
Anmol Gupta <2k23csds2310397@gmail.com>
2026-05-03 22:58:44 +0530

    Time: 0 ms (100%), Space: 42.6 MB (85.99%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length())
-               return false;
-        String concat=s+s;
-        if(concat.contains(goal))
+        if(s.length()!=goal.length())return false;
+        s+=s;
+        if(s.contains(goal)){
             return true;
+        }
         else
-           return  false;
-        
+        return false;
     }
 }
