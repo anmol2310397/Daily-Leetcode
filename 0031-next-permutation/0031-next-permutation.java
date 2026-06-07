6dba2b45cb78737486b8830eee71b87ee32b02a3
Anmol Gupta <2k23csds2310397@gmail.com>
2026-06-07 23:14:52 +0530

    Time: 0 ms (100%), Space: 44.5 MB (67.39%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,40 +1,31 @@
-class Solution {
-    public void swap(int[] arr, int index1, int index2) {
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
     }
 
-    public void nextPermutation(int[] nums) {
-        int len = nums.length;
-        int pivot = -1;
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
 
-       
-        for (int i = len - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                pivot = i;
-                break;
-            }
-        }
-        if (pivot == -1) {
-            for (int i = 0; i < len / 2; i++) {
-                swap(nums, i, len - i - 1);
-            }
-            return;
-        }
-
-        // Step 3: Find the smallest element in the suffix larger than nums[pivot]
-        for (int i = len - 1; i > pivot; i--) {
-            if (nums[i] > nums[pivot]) {
-                swap(nums, pivot, i);
-                break;
-            }
-        }
-        int left = pivot + 1, right = len - 1;
-        while (left < right) {
-            swap(nums, left, right);
-            left++;
-            right--;
-        }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
